461dc4d25317a175a90e08f0f69017f607b3d889
erika.silva7 <79651321+erika393@users.noreply.github.com>
2022-07-26 17:51:49 -0300

    first commit

---
+++ NotifyLib/Notify.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.IO;  
+using System.Net;  
+using Newtonsoft.Json;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit.Text;
+using System.Collections;
+
+namespace NotifyLib.Notify;
+    public class Notify
+    {
+        public void SendEmail(string sender, string passwordSender, string recipient, string subject, string message, ArrayList attachments)
+        {
+            //create email
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(sender));
+            email.To.Add(MailboxAddress.Parse(recipient));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = message };
+
+            //attachments
+            var builder = new BodyBuilder();
+            builder.TextBody = message;
+            foreach(string obj in attachments){
+                builder.Attachments.Add(obj);
+            }
+            email.Body = builder.ToMessageBody();
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtpout.secureserver.net", 465, SecureSocketOptions.Auto);
+            smtp.Authenticate(sender, passwordSender);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+            
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using NotifyLib.Notify;
+
+namespace NotifyEmail;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Notify not = new Notify();
+            ArrayList attachments = new ArrayList();
+            try{
+                Console.Write("Sender: ");
+                string sender = Console.ReadLine();
+                Console.Write("Password Sender: ");
+                string passwordSender = Console.ReadLine();
+                Console.Write("Recipient: ");
+                string recipient = Console.ReadLine();
+                Console.Write("Subject: ");
+                string subject = Console.ReadLine();
+                Console.Write("Message: ");
+                string message = Console.ReadLine();
+                Console.Write("Do you to add attachments with your body of email? (Y/N) ");
+                char answer = char.Parse(Console.ReadLine().ToUpper());
+                if(answer == 'Y')
+                {
+                    char stop = 'N';
+                    do
+                    {
+                        Console.WriteLine("Type the path: example (C:\\...)");
+                        string path = Console.ReadLine();
+                        attachments.Add(path);
+                        Console.WriteLine("Add again? (Y/N)");
+                        stop = char.Parse(Console.ReadLine().ToUpper());
+                    }while(stop == 'Y');
+                }
+
+                not.SendEmail(sender,passwordSender,recipient,subject,message, attachments);
+            }
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
